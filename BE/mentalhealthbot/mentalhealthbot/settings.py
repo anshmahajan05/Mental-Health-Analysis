bcc716868c1851f5a0636d77f87d63bce62c3522
Ansh Mahajan <119053203+anshmahajan05@users.noreply.github.com>
2023-10-28 10:15:44 +0530

    postgres sql connections for migrate

--- BE/mentalhealthbot/mentalhealthbot/settings.py
+++ BE/mentalhealthbot/mentalhealthbot/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "analysisapp",
 ]
 
 MIDDLEWARE = [
@@ -73,10 +74,21 @@ WSGI_APPLICATION = "mentalhealthbot.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'MentalHealthBot',
+        'USER': 'postgres',
+        'PASSWORD': '322Ansh@',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
