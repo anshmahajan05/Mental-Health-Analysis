2b17fd5ad7e7af5431fef3dbcd6641c2046b14c8
Ansh Mahajan <119053203+anshmahajan05@users.noreply.github.com>
2023-10-29 22:29:37 +0530

    done

--- BE/mentalhealthbot/mentalhealthbot/settings.py
+++ BE/mentalhealthbot/mentalhealthbot/settings.py
@@ -157,10 +157,14 @@ REST_FRAMEWORK = {
     ],
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'anshmahajan051002@gmail.com'
-EMAIL_HOST_PASSWORD = 'ykiouvvmrcwtjnhb'
-DEFAULT_FROM_EMAIL = 'anshmahajan051002@gmail.com'
+EMAIL_HOST_USER = "anshmahajan051002@gmail.com"
+EMAIL_HOST_PASSWORD = "ykiouvvmrcwtjnhb"
+DEFAULT_FROM_EMAIL = "anshmahajan051002@gmail.com"
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
