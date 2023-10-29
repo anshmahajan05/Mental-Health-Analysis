6d15ed0427971c0eea1cb6dd99c179d01a3f4b26
Neha Bhavsar <125593320+NehaBhavsar102@users.noreply.github.com>
2023-10-29 13:09:54 +0530

    "Subscription Table Added"

--- BE/mentalhealthbot/analysisapp/urls.py
+++ BE/mentalhealthbot/analysisapp/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path("login/", login.as_view(), name="login"),
     path("logout/", logout.as_view(), name="logout"),
     path("signup/", signup.as_view(), name="signup"),
+<<<<<<< Updated upstream
     path("Booking/", Booking.as_view(), name="Booking"),
+=======
+    path("subscriptiontable/", subscriptiontable.as_view(), name="subscriptiontable"),
+>>>>>>> Stashed changes
 ]
--- BE/mentalhealthbot/analysisapp/views.py
+++ BE/mentalhealthbot/analysisapp/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+import pandas as pd
 from django.http import JsonResponse
 from rest_framework import status
 from rest_framework.permissions import IsAdminUser
@@ -100,6 +102,7 @@ class signup(APIView):
         )
         return Response({"message": "New User Created"}, status=status.HTTP_200_OK)
 
+<<<<<<< Updated upstream
 
 @authentication_classes([JWTAuthentication])
 @permission_classes([IsAuthenticated])
@@ -190,3 +193,47 @@ class Booking(APIView):
         else:
             context["Error"] = "User type doesnot exist"
         return JsonResponse(context)
+=======
+@authentication_classes([JWTAuthentication])
+@permission_classes([IsAuthenticated])
+class subscriptiontable(APIView):
+    def post(self,request):
+        UserId=request.user.id
+        SubscriptionType=request.data.get("SubscriptionType")
+        StartDate= datetime.now().date()
+        EndDate = StartDate + timedelta(days=30)
+        user_instance = Mst_UsrTbl.objects.get(id=UserId)
+        subscription = SubscriptionTable.objects.create(
+            UserId=user_instance,
+
+            )
+        subscription.StartDate=StartDate
+        subscription.EndDate=EndDate
+        subscription.SubscriptionType=SubscriptionType
+       # subscription=SubscriptionTable.create(
+          #  UserId=user_instance,
+        #     SubscriptionType=SubscriptionType,
+        #     StartDate=StartDate,
+        #     EndDate=EndDate,
+        # )
+        subscription.save()
+        
+
+        return Response({"message" : "Subscription Added"},status=status.HTTP_200_OK)
+    
+    def get(self,request):
+        context={}
+        UserId=request.user.id
+        User_obj = pd.DataFrame(
+            SubscriptionTable.objects.filter(UserId=UserId).values(
+                "SubscriptionId","SubscriptionType","StartDate","EndDate"
+            )
+        )
+        context["Subscription"] = User_obj.to_dict(orient="records")
+        return JsonResponse(context,status=status.HTTP_200_OK)
+    
+    
+        
+
+
+>>>>>>> Stashed changes
