1ac8b50b307f08a91c0d96e91dcd04e24011620d
Ansh Mahajan <119053203+anshmahajan05@users.noreply.github.com>
2023-11-03 22:54:37 +0530

    done

--- FE/MentalHealthBot/src/components/client/SubscriptionView.jsx
+++ FE/MentalHealthBot/src/components/client/SubscriptionView.jsx
@@ -1,22 +1,16 @@
-
-import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
-import PlanCards from './PlanCards';
-import Sidebar from './Sidebar';
-import './Subscription.css'
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import PlanCards from "./PlanCards";
+import Sidebar from "./Sidebar";
+import "./Subscription.css";
 
 function SubscriptionView() {
-  
-
   return (
-
-    <div className="subscription-container">
-        <Sidebar />
-        <PlanCards/>
-       
+    <div className='subscription-container'>
+      <Sidebar />
+      <PlanCards />
     </div>
-
-  )
+  );
 }
 
 export default SubscriptionView;
