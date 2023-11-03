daed6227fe33ba381989ddbdf7f053c5176d3b77
xVaINx <ayushsp19@gmail.com>
2023-11-04 00:07:39 +0545

    updated apexcharts

--- FE/MentalHealthBot/src/components/pages/Dashboard.jsx
+++ FE/MentalHealthBot/src/components/pages/Dashboard.jsx
@@ -1,36 +1,12 @@
 import NavBar from "../client/NavBar";
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import ProfileCard from "../client/ProfileCard";
-import { Chart } from "react-google-charts";
-
-export const data = [
-    ["Year", "Sales", "Expenses"],
-    ["2004", 1000, 400],
-    ["2005", 1170, 460],
-    ["2006", 660, 1120],
-    ["2007", 1030, 540],
-  ];
-  
-  export const options = {
-    title: "Company Performance",
-    curveType: "function",
-    legend: { position: "bottom" },
-  };
-  export const data1 = [
-    ["Task", "Hours per Day"],
-    ["Work", 11],
-    ["Eat", 2],
-    ["Commute", 2],
-    ["Watch TV", 2],
-    ["Sleep", 7],
-  ];
-  
-  export const options1 = {
-    title: "My Daily Activities",
-  };
-  
+import BarChart from "../charts/BarChart";
+import LineChart from "../charts/lineChart";
 
 const Login = () => {
+  
+
   return (
     <>
       <NavBar />
@@ -47,22 +23,10 @@ const Login = () => {
       >
         <div style={{ display:"flex", padding: "5px", flexDirection: "row", flexWrap:"wrap", margin:"auto", justifyContent:"center" }}>
           <div className="dashboard-chart">
-          <Chart
-            chartType="AreaChart"
-            width="100%"
-            height="300px"
-            data={data}
-            options={options}
-            />
+            <BarChart />
           </div>
           <div className="dashboard-chart">
-          <Chart
-            chartType="PieChart"
-            data={data1}
-            options={options1}
-            width={"100%"}
-            height={"300px"}
-            />
+            <LineChart />
             </div>
         </div>
         <div style={{ border:"none", height:"300px", flexWrap:"wrap", width:"100%" }} className="dashboard-chart">
