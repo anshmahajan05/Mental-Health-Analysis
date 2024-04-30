7e985bc082aaf078390a50f60870de36fc2faeec
xVaINx <ayushsp19@gmail.com>
2024-04-30 18:09:50 +0530

    dashboard confilict resolved

--- FE/MentalHealthBot/src/components/pages/Dashboard.jsx
+++ FE/MentalHealthBot/src/components/pages/Dashboard.jsx
@@ -2,9 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import useAuth from "../../../utils/useAuth";
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
-// import ProfileCard from "../client/ProfileCard";
-// import BarChart from "../charts/BarChart";
-// import LineChart from "../charts/lineChart";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Dashboard = () => {
   const { token, authLogout } = useAuth();
@@ -59,23 +57,6 @@ const Dashboard = () => {
           width: "95%",
         }}
       >
-        {/* <div style={{ display:"flex", padding: "5px", flexDirection: "row", flexWrap:"wrap", margin:"auto", justifyContent:"center" }}>
-          <div className="dashboard-chart">
-            {/* <BarChart /> */}
-          </div>
-          <div className="dashboard-chart">
-            {/* <LineChart /> */}
-            </div>
-        </div>
-        <div style={{ border:"none", height:"300px", flexWrap:"wrap", width:"100%" }} className="dashboard-chart">
-            <ProfileCard name={"Dr. Ayush Kumar"} qual={"MD. MS."}/>
-            <ProfileCard name={"Dr. Ansh Mahajan"} qual={"MD. MS."}/>
-            <ProfileCard name={"Dr. Neha Bhavsar"} qual={"MD. MS."}/>
-            <ProfileCard name={"Dr. Swarda Mashere"} qual={"MD. MS."}/>
-        </div>
-        <div style={{ display:"flex", border:"solid",margin:"0px", width:"100%"}} className="dashboard-chart">
-            User Data
-        </div> */}
         {renderUserButtons()}
         {userType === "user" && renderUserButtons()}
         {userType === "therapist" && renderTherapistButtons()}
