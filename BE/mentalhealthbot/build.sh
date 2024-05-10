79fc8b31ad77d4fb0506c927372f1f259debf094
Ansh Mahajan <anshmahajan05102002@gmail.com>
2024-05-10 17:35:42 +0000

    done

---
+++ BE/mentalhealthbot/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Build the project
+echo "Building the project..."
+python3.9 -m pip install -r requirements.txt
+
+echo "Collect Static..."
+python3.9 manage.py collectstatic --noinput --clear
