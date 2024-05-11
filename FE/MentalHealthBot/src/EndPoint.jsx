a010b638d349a119823cef6ffd1ef228f16e4f52
Ansh Mahajan <anshmahajan05102002@gmail.com>
2024-05-11 12:37:53 +0530

    backend endpoint changes

--- FE/MentalHealthBot/src/EndPoint.jsx
+++ FE/MentalHealthBot/src/EndPoint.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react-refresh/only-export-components */
 // const URL = "http://127.0.0.1:8000/";
-const URL = "http://172.208.81.190/";
+// const URL = "http://172.208.81.190/";
+const URL = "https://mental-health-analysis.onrender.com/"
 
 export default URL;
